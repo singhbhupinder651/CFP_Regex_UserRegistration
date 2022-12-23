d69a923c6673c12c6763639eac45536572053f56
singhbhupinder651 <71844883+singhbhupinder651@users.noreply.github.com>
2022-12-23 12:23:01 +0530

    [Bhupinder Singh] Add : Unit testing to validate last name

---
+++ test_User_Registration.py
@@ -0,0 +1,34 @@
+'''
+    @Author: Bhupinder Singh
+    @Date: 22-12-2022
+    @Last Modified by: Bhupinder Singh
+    @Last Modified date: 22-12-2022
+    @Title: Unit testing of test cases for user registration
+'''
+
+import unittest
+from User_Registration import UserRegistration
+
+
+user_obj = UserRegistration()
+
+
+class TestUserRegistration(unittest.TestCase):
+    # start with capital letter and minimum 3 chars.
+    def test_to_validate_first_name(self):
+        self.assertTrue(user_obj.get_first_name('Bhupinder'))
+        self.assertFalse(user_obj.get_first_name('bhupinder'))
+        self.assertTrue(user_obj.get_first_name('Bhu'))
+        self.assertFalse(user_obj.get_first_name('Bh'))
+        
+
+    # start with capital letter and minimum 3 chars.
+    def test_to_validate_last_name(self):
+        self.assertTrue(user_obj.get_last_name('Singh'))
+        self.assertFalse(user_obj.get_last_name('singh'))
+        self.assertTrue(user_obj.get_last_name('Sin'))
+        self.assertFalse(user_obj.get_last_name('Si'))
+
+
+if __name__ == '__main__':
+    unittest.main()
